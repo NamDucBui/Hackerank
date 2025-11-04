5cce32889fcfdc75990b88052f8a46bdfa2d191b
Nam Duc Bui <buiducnam2002@gmail.com>
2025-11-04 16:05:46 +0700

    4.11.2025

---
+++ 4.11.2025/1.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function gcd(a,b) {
+    while (b !== 0){
+        let temp = b;
+        b = a%b;
+        a= temp;
+    }
+    return a;
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+}
+
+function getGcd(arr) {
+    let result = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        result = gcd(result, arr[i]);
+    }
+    return result;
+}
+
+function getLcm(arr) {
+    let result = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        result = lcm(result, arr[i]);
+    }
+    return result;
+}
+
+
+
+function getTotalX(a, b) {
+    // Write your code here
+    
+    const lcmA = getLcm(a);
+    
+    const gcdB = getGcd(b);
+    
+    
+    let count = 0;
+    for (let x = lcmA; x <= gcdB; x += lcmA) {
+        if (gcdB % x === 0) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const brr = readLine().replace(/\s+$/g, '').split(' ').map(brrTemp => parseInt(brrTemp, 10));
+
+    const total = getTotalX(arr, brr);
+
+    ws.write(total + '\n');
+
+    ws.end();
+}
---
+++ 4.11.2025/6.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthday' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
+
+function birthday(s, d, m) {
+    // Write your code here
+    if (m > s.length) return 0;
+    
+    let count = 0;
+    
+    
+    let sum = 0;
+    for (let i = 0; i < m; i++) {
+        sum += s[i];
+    }
+    
+    
+    if (sum === d) count++;
+    
+    
+    for (let i = m; i < s.length; i++) {
+        sum = sum - s[i - m] + s[i]; 
+        if (sum === d) count++; 
+    }
+    
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    const result = birthday(s, d, m);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}

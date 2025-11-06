0fd6ce1b2093e0fdf37a3ff401edb6e0dff92f50
Nam Duc Bui <buiducnam2002@gmail.com>
2025-11-06 18:03:27 +0700

    6.11.2025

---
+++ 6.11.2025/4.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'designerPdfViewer' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h
+ *  2. STRING word
+ */
+
+function designerPdfViewer(h, word) {
+    // Write your code here
+    const maxHeight = word
+        .split('') 
+        .map(char => h[char.charCodeAt(0) - 97]) 
+        .reduce((max, curr) => Math.max(max, curr), 0); 
+    
+    
+    return maxHeight * word.length;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const h = readLine().replace(/\s+$/g, '').split(' ').map(hTemp => parseInt(hTemp, 10));
+
+    const word = readLine();
+
+    const result = designerPdfViewer(h, word);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
